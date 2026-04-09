b4a451464ee8fe4523925fe9093127537cb8281b
Ayan Das <1998ayandas@gmail.com>
2026-04-09 14:41:36 +0530

    Updated comments

--- scripts/weather_api.py
+++ scripts/weather_api.py
@@ -3,10 +3,7 @@ import requests
 
 
 def get_weather_data(lat, lon, start_date, end_date):
-    # lat=52.52
-    # lon=13.405
-    # start_date="2026-04-05"
-    # end_date="2026-04-07"#*/
+
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum"
     response = requests.get(url)
     data = response.json()
